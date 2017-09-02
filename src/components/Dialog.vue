a377731c9a5025b49d6cedad70f8b1150a690c41
soloJiang <350740249@qq.com>
2017-09-03 00:50:19 +0800

    first commit

---
+++ src/components/Dialog.vue
@@ -0,0 +1,138 @@
+<template>
+  <transition name="dialog">
+    <div class="dialog" @click="closeModal" v-show="visible">
+      <div class="dialog_drop" ref="others"></div>
+      <div class="close" @click="close" :class="leave ? 'dialog_leave' : 'dialog_enter'">×</div>
+      <div class="dialog_wrapper" :class="leave ? 'dialog_leave' : 'dialog_enter'">
+        <div class="dialog_header">{{title}}</div>
+        <div class="dialog_description">{{description}}</div>
+        <div class="dialog_body">
+          <slot name="body"></slot>
+        </div>
+        <div class="dialog_footer">
+          <slot name="footer"></slot>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'dialog',
+    data () {
+      return {
+        leave: false
+      }
+    },
+    props: {
+      visible: {
+        type: Boolean,
+        defalut: false
+      },
+      title: {
+        type: String,
+        defalut: ''
+      },
+      description: {
+        type: String,
+        default: ''
+      }
+    },
+    methods: {
+      close () {
+        this.leave = true
+        this.$emit('update:visible', false)
+      },
+      closeModal (e) {
+        if (this.visible) {
+          const others = this.$refs.others
+          e.target.contains(others) ? this.close() : ''
+        }
+      }
+    },
+    watch: {
+      visible (newVal) {
+        this.leave = newVal ? false : true
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .dialog-leave-active
+    opacity 0
+    transition opacity .8s ease-out
+  .dialog
+    position fixed
+    top 0px
+    left 0px
+    bottom 0px
+    right 0px
+    z-index 100
+    .dialog_drop
+      position absolute
+      top 0px
+      left 0px
+      bottom 0px
+      right 0px
+      z-index 0
+      background rgba(0,0,0,.65)
+    .close
+      z-index 1
+      color #ffffff
+      position fixed
+      top 0
+      margin-top 15%
+      left calc(50% + 280px)
+      font-size 40px
+      cursor pointer
+    .dialog_leave
+      transform translateY(0)
+      animation leave .8s ease forwards
+    .dialog_enter
+      transform translateY(100px)
+      animation wrapper .8s ease forwards
+    .dialog_wrapper
+      z-index 1
+      position relative
+      margin 0 auto
+      margin-top 15%
+      min-height 200px
+      width 0
+      background-color #fff
+      overflow hidden
+      padding 20px
+      box-sizing border-box
+      width: 500px
+      .dialog_header
+        white-space nowrap
+        text-align center
+        font-size 18px
+        margin-bottom 20px
+        font-weight bold
+      .dialog_body
+        margin 40px 0
+      .dialog_footer
+        margin-top 30px
+  @keyframes wrapper {
+    from {
+      opacity 0
+    }
+    to {
+      opacity 1
+      transform translateY(0)
+    }
+  }
+  @keyframes leave {
+    from {
+      opacity 1
+    }
+    to {
+      opacity 0
+      transform translateY(100px)
+    }
+  }
+</style>
+
+
---
+++ src/components/Hello.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <button @click="show = !show">Modal测试</button>
+    <s-dialog :title="'添加到论文集'" :visible.sync="show">
+      <div slot="body">
+        <div align="center">你可以创建多个论文集，将你喜欢的论文分类收藏</div>
+      </div>
+      <div slot="footer">
+        <button>确定</button>
+      </div>
+    </s-dialog>
+  </div>
+</template>
+
+<script>
+import dialog from './Dialog'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      show: false
+    }
+  },
+  components: {
+    "s-dialog": dialog 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
